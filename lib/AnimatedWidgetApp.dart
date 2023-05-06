2d739d959a722b522be2572d2e512112a8a7774d
Simba Classes <strikewithgoal2000@gmail.com>
2023-05-06 19:33:07 +0530

    Commit Message

--- lib/AnimatedWidgetApp.dart
+++ lib/AnimatedWidgetApp.dart
@@ -1,13 +1,69 @@
-class  extends StatefulWidget {
-  const ({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
+
+class MakeOwnAnimationWidgetApp extends StatefulWidget {
+  const MakeOwnAnimationWidgetApp({Key? key}) : super(key: key);
 
   @override
-  State<> createState() => _State();
+  State<MakeOwnAnimationWidgetApp> createState() =>
+      _MakeOwnAnimationWidgetAppState();
 }
 
-class _State extends State<> {
+class _MakeOwnAnimationWidgetAppState extends State<MakeOwnAnimationWidgetApp>
+    with SingleTickerProviderStateMixin {
+  AnimationController? Mycontroller;
+  Animation<double>? sizeAnimation;
+
+  @override
+  void initState() {
+    Mycontroller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    sizeAnimation = Tween<double>(
+      begin: 100,
+      end: 200,
+    ).animate(Mycontroller!);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Make Your Own Animation App'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            _MyAnimatedContainer(
+              sizeAnimation: sizeAnimation,
+            ),
+            ElevatedButton(
+              onPressed: () => Mycontroller?.forward(),
+              child: Text('Forward Animation'),
+            ),
+            ElevatedButton(
+              onPressed: () => Mycontroller?.reverse(),
+              child: Text('Reverse Animation'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MyAnimatedContainer extends AnimatedWidget {
+  _MyAnimatedContainer({Animation<double>? sizeAnimation})
+      : super(listenable: sizeAnimation!);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> sizeAnimation = listenable as Animation<double>;
+
+    return Container(
+      child: Image.network(
+          'https://thumbs.dreamstime.com/b/creative-mind-concept-hand-holding-colorful-brain-sketch-concrete-background-85126393.jpg'),
+      height: sizeAnimation!.value,
+      width: sizeAnimation!.value,
+      padding: EdgeInsets.all(15),
+    );
   }
 }
