b0e3e277bf3ef73bb7b9b38f596f90ae8044b70f
Simba Classes <strikewithgoal2000@gmail.com>
2023-05-08 19:57:11 +0530

    Simba Classes 08/05/2023

--- lib/lootieApp.dart
+++ lib/lootieApp.dart
@@ -8,14 +8,43 @@ class LottieuseApp extends StatefulWidget {
   State<LottieuseApp> createState() => _LottieuseAppState();
 }
 
-class _LottieuseAppState extends State<LottieuseApp> {
+class _LottieuseAppState extends State<LottieuseApp>
+    with SingleTickerProviderStateMixin {
+  AnimationController? ltcontroller;
+
+  @override
+  void initState() {
+    super.initState();
+    ltcontroller = AnimationController(vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('lotti Use App'),
       ),
-      body: Center(child: Lottie.asset('assets/laughing.json')),
+      body: Column(
+        children: [
+          Container(
+            child: Lottie.asset('assets/laughing.json'),
+          ),
+          ButtonBar(
+            children: [
+              IconButton(
+                  onPressed: ltcontroller!.isCompleted
+                      ? null
+                      : () => ltcontroller!.forward(),
+                  icon: Icon(Icons.skip_next)),
+              IconButton(
+                  onPressed: ltcontroller!.isAnimating
+                      ? () => ltcontroller!.repeat()
+                      : () => ltcontroller!.stop(),
+                  icon: Icon(Icons.play_arrow)),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
